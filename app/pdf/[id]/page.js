c99749c533a27d2b760547c7053f375df93f0d6c
Desmet-Lars <desmetlars5@gmail.com>
2024-08-30 16:41:32 +0200

    trying to fix multiple pages

--- app/pdf/[id]/page.js
+++ app/pdf/[id]/page.js
@@ -6,6 +6,7 @@ import { firestore } from '@/lib/FirebaseConfig';
 import { doc, getDoc, collection, addDoc, getDocs } from 'firebase/firestore';
 import { Container, Typography, Button, TextField, Box, Card, CardContent, CircularProgress } from '@mui/material';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { Document, Page } from 'react-pdf';
 
 // Define a custom theme (you can customize this as needed)
 const theme = createTheme({
@@ -90,16 +91,22 @@ export default function PdfViewer() {
                                 <Typography variant="h4" gutterBottom color="primary">
                                     {pdf.title}
                                 </Typography>
-                                <iframe
-                                    src={pdf.url}
-                                    title="PDF Viewer"
-                                    style={{
-                                        width: '100%',
-                                        height: '120vh', // Adjust as needed
-                                        border: 'none',
-                                        overflow: 'auto', // Enable scrolling within the iframe
-                                    }}
-                                />
+                                <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                                    <Document
+                                        file={pdf}
+                                        onLoadSuccess={onDocumentLoadSuccess}
+                                        options={{ cMapUrl: 'cmaps/', cMapPacked: true }}
+                                    >
+                                        <Page pageNumber={pageNumber} />
+                                    </Document>
+                                    <Box sx={{ mt: 2 }}>
+                                        {numPages && (
+                                            <Typography variant="body1">
+                                                Page {pageNumber} of {numPages}
+                                            </Typography>
+                                        )}
+                                    </Box>
+                                </Box>
 
 
                                 <Box sx={{ mt: 4 }}>

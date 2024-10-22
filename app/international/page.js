182a3e0b1cef6cc4c5d63e8ed7915a2083102092
Desmet-Lars <desmetlars5@gmail.com>
2024-10-22 11:30:44 +0200

    limited acces to only BE and NL

---
+++ app/international/page.js
@@ -0,0 +1,8 @@
+export default function InternationalPage() {
+    return (
+      <div>
+        <h1>International Page</h1>
+        <p style={{color:'red'}}>You are not allowed to visit this website</p>
+      </div>
+    )
+  }
